59fe74f5b7b3838d4e95813b8d7d1c2516484152
jrjjxg <2902756263@qq.com>
2025-03-14 00:04:27 +0800

    3.14

--- demo1/src/main/java/com/advisor/mapper/TestTypeMapper.java
+++
@@ -1,9 +0,0 @@
-package com.advisor.mapper;
-
-import com.advisor.entity.TestType;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TestTypeMapper extends BaseMapper<TestType> {
-}
